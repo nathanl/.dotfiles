69ac106b1565f594dc37ccf80c92cddbe908f8f6
Nathan Long <him@nathanmlong.com>
2016-04-18 11:12:19 -0400

    Add a setup script

---
+++ new_machine_setup.rb
@@ -0,0 +1,40 @@
+# execute mkdir ~/.bundle
+
+%x{mkdir ~/.bundle} unless File.exist?(File.expand_path("~/.bundle"))
+
+FILES = {
+  "zsh"                   => ".zsh",
+  "zsh/zshrc"             => ".zshrc",
+  "zsh/zshenv"            => ".zshenv",
+
+  "tmux.conf"             => ".tmux.conf",
+
+  "vim"                   => ".vim",
+  "vim/vimrc"             => ".vimrc",
+  "vim/vimrc_simple"      => ".vimrc_simple",
+  "vim/vimrc_barebones"   => ".vimrc_barebones",
+  "vim/gvimrc"            => ".gvimrc",
+
+  "git/gitconfig"         => ".gitconfig",
+  "git/gitignore"         => ".gitignore",
+
+  "psqlrc"                => ".psqlrc",
+
+  "irbrc"                 => ".irbrc",
+  "pryrc"                 => ".pryrc",
+  "bundle_config"         => ".bundle/config",
+  "iex.exs"               => ".iex.exs",
+}
+
+FILES.each do |original_file, symlink_location|
+  source = File.expand_path("~/.dotfiles/#{original_file}")
+  dest   = File.expand_path("~/#{symlink_location}")
+  if File.exist?(dest)
+    puts "exists: #{dest}"
+  else
+    result = system("ln -s #{source} #{dest}")
+    puts "symlinking #{source} -> #{dest} #{result ? "worked" : "failed"}"
+  end
+end
+
+puts "don't forget to do the stuff in ~/.dotfiles/ssh_config"
